e76f8eb96aa1a102fa45213bb1bb23f853a2b19a
Justin Daniel <justinddaniel@gmail.com>
2017-11-17 22:19:25 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,13 +10,12 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @post = Post.find(params[:id])
   end
 
   # GET /posts/new
   def new
     @post = Post.new
-    @tag = Tag.new
+    @tags = @post.tags.build
   end
 
   # GET /posts/1/edit
